dce42b7bc8d18d1ce0538aae153ac3cc30e041f7
Bheema Nitish <nitishbheema@gmail.com>
2026-03-01 13:04:16 +0530

    Refactor database connection to use environment variables

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,13 @@ if(!isset($_SESSION['user_id'])){
     exit();
 }
 
-$conn = new mysqli("localhost","root","","trackwise");
+$conn = new mysqli(
+    $_ENV['MYSQLHOST'],
+    $_ENV['MYSQLUSER'],
+    $_ENV['MYSQLPASSWORD'],
+    $_ENV['MYSQLDATABASE'],
+    $_ENV['MYSQLPORT']
+);
 $uid = $_SESSION['user_id'];
 
 $userData = $conn->query("SELECT name,budget,photo FROM users WHERE user_id=$uid")->fetch_assoc();
