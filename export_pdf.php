9a06c608336cf3a4d3b3d8201f9df74497d531ee
Bheema Nitish <nitishbheema@gmail.com>
2026-03-01 13:18:08 +0530

    Update database connection parameters in export_pdf.php

--- export_pdf.php
+++ export_pdf.php
@@ -2,13 +2,17 @@
 session_start();
 require('fpdf/fpdf.php');
 
-$conn = new mysqli(
-    $_ENV['MYSQLHOST'],
-    $_ENV['MYSQLUSER'],
-    $_ENV['MYSQLPASSWORD'],
-    $_ENV['MYSQLDATABASE'],
-    $_ENV['MYSQLPORT']
-);
+$host = "yamabiko.proxy.rlwy.net";
+$user = "root";
+$password = "YOUR_PASSWORD_HERE";
+$database = "railway";
+$port = 15951;
+
+$conn = new mysqli($host, $user, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 $uid = $_SESSION['user_id'];
 
 $pdf = new FPDF();
